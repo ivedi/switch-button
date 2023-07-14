487e91b93d8c8aaf55199c37d3ec97d10eb06ebb
ivedi <halil.ivedi@ctengineering.de>
2023-07-14 20:04:18 +0300

    created SwitchButton.cpp and SwitchButton.h files

---
+++ SwitchButton.cpp
@@ -0,0 +1,19 @@
+/*
+ * SwitchButton.cpp - Basic switch button library.
+ * Created by Halil İbrahim İvedi, July 14, 2023.
+ * */
+
+#include "Arduino.h"
+#include "SwitchButton.h"
+
+SwitchButton::SwitchButton(byte buttonPin) {
+  // TODO
+}
+
+void SwitchButton::begin() {
+  // TODO
+}
+
+bool update(byte debounceTime = 50) {
+  // TODO
+}
---
+++ SwitchButton.h
@@ -0,0 +1,23 @@
+/*
+ * SwitchButton.h - Basic switch button library.
+ * Created by Halil İbrahim İvedi, July 14, 2023.
+ * */
+#ifndef SwitchButton_h
+#define SwitchButton_h
+
+#include "Arduino.h"
+
+class SwitchButton
+{
+  public:
+    SwitchButton(byte buttonPin);
+    byte buttonPin;
+    bool pressed;
+    void begin();
+    bool update(byte debounceTime = 50);
+  private:
+    int _currentState;
+    int _lastFlickerableState;
+    int _lastSteadyState;
+    unsigned long _lastDebounceTime;
+}
